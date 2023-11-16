c6527558983034dce20a1acfafa5aa7e1221155a
Tim Bannister <tim@scalefactory.com>
2023-11-16 10:50:10 +0000

    improvement: Revise version requirements
    - Require Terraform 1.0
    - Use a newer provider plugin for Terraform Cloud

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.6, < 2.0"
+  required_version = "~> 1.0"
 
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
-      version = "~> 0.48.0"
+      version = "~> 0.50.0"
     }
   }
 }
